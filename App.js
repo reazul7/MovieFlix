84963cccbe5a026d7222a7893a3c9e7a7aa5cf56
reazul7 <islamreazul7777@gmail.com>
2021-06-04 22:55:04 +0600

    problem and design fix

--- App.js
+++ App.js
@@ -1,64 +1,127 @@
-import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import {MaterialCommunityIcons} from '@expo/vector-icons'
-
-import Home from './screens/home'
-import Profile from './screens/profile'
-import Recents from './screens/recents'
-
-const Tab = createMaterialBottomTabNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-    <Tab.Navigator 
-      initialRoute = 'Home'
-      activeColor = "#02ad94"
-      inactiveColor = "#dedede"
-      style = {{backgroundColor: '#000'}}
-      barStyle = {{backgroundColor: '#0f0f0f', padding: 4}}
-    >
-      <Tab.Screen 
-        name="Home"
-        component={Home} 
-        options={{
-          tabBarLabel: '',
-          tabBarIcon: ({color}) => (
-            <MaterialCommunityIcons name='home' color={color} size={28}/>
-          )
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  ImageBackground,
+  StatusBar,
+  ScrollView,
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+export class App extends Component {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
         }}
-        />
-      <Tab.Screen
-         name="Profile" 
-         component={Profile} 
-         options={{
-          tabBarLabel: '',
-          tabBarIcon: ({color}) => (
-            <MaterialCommunityIcons name='camera-metering-spot' color={color} size={28}/>
-          )
-        }}
-         />
-      <Tab.Screen 
-        name="Recents" 
-        component={Recents} 
-        options={{
-          tabBarLabel: '',
-          tabBarIcon: ({color}) => (
-            <MaterialCommunityIcons name='account' color={color} size={28}/>
-          )
-        }}
-        />
-    </Tab.Navigator>
-    </NavigationContainer>
-  );
+      >
+        <StatusBar hidden />
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View
+            style={{
+              overflow: "hidden",
+              borderBottomRightRadius: 50,
+            }}
+          >
+            <ImageBackground
+              style={{
+                width: "100%",
+                height: 250,
+              }}
+              source={{
+                uri:
+                  "https://png.pngtree.com/thumb_back/fh260/back_pic/03/54/58/35579acf3576e9d.jpg",
+              }}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  paddingHorizontal: 20,
+                  marginTop: 20,
+                }}
+              >
+                <View>
+                  <MaterialCommunityIcons name="text" size={24} color="white" />
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                  }}
+                >
+                  <View
+                    style={{
+                      marginRight: 20,
+                    }}
+                  >
+                    <Feather name="bell" size={22} color="white" />
+                  </View>
+                  <View>
+                    <Feather name="search" size={22} color="white" />
+                  </View>
+                </View>
+              </View>
+              <View
+                style={{
+                  paddingHorizontal: 20,
+                  flex: 1,
+                  marginTop: -15,
+                  justifyContent: "center",
+                }}
+              >
+                <Text
+                  style={{
+                    fontSize: 18,
+                    color: "white",
+                    fontWeight: "bold",
+                  }}
+                >
+                  Hello Movie Lovers People,
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 12,
+                    color: "white",
+                    fontWeight: "bold",
+                  }}
+                >
+                  Hope you have a great day!
+                </Text>
+                <View
+                  style={{
+                    flexDirection: "row",
+                  }}
+                >
+                  <View
+                    style={{
+                      paddingHorizontal: 10,
+                      paddingVertical: 5,
+                      borderRadius: 10,
+                      marginTop: 5,
+                      backgroundColor: "grey",
+                    }}
+                  >
+                    <Text
+                      style={{
+                        fontSize: 10,
+                        color: "white",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      VIP MEMBERSHIP
+                    </Text>
+                  </View>
+                </View>
+              </View>
+            </ImageBackground>
+          </View>
+        {/* end home banner */}
+        </ScrollView>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
