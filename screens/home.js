750c08756ed643a506ad47c66a3d802383ac5ce6
reazul7 <islamreazul7777@gmail.com>
2021-06-04 18:58:53 +0600

    call first img

--- screens/home.js
+++ screens/home.js
@@ -1,22 +1,61 @@
-import * as React from 'react';
-import {StyleSheet, Text, View} from 'react-native'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  Dimensions,
+  StatusBar,
+  ImageBackground,
+  TextInput,
+  FlatList,
+} from "react-native";
+import React, { useRef, useState, useEffect } from "react";
+import Carousel from "react-native-anchor-carousel";
+import { FontAwesome5, Feather, MaterialIcons } from "@expo/vector-icons";
 
 const home = () => {
-    return (
-        <View style={styles.container}>
-            <Text>This is home</Text>
+  const [background, setBackground] = useState({
+    uri: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQA_-tL18_rj9zEcjN6n41NEaJm-kRNF9UeOtvksZ4z_OW6jRA9",
+    name: "Avengers: End Game",
+    stat: "2019 ‧ Action/Sci-fi ‧ 3h 2m",
+    desc: "After Thanos, an intergalactic warlord, disintegrates half of the universe, the Avengers must reunite and assemble again to reinvigorate their trounced allies and restore balance.",
+  });
+
+  return (
+    <ScrollView>
+      <View style={styles.carouselContentContainer}>
+        <View style={{ ...StyleSheet.absoluteFill, backgroundColor: "#000" }}>
+          <ImageBackground
+            source={{ uri: background.uri  }}
+            style={styles.ImageBg}
+          ></ImageBackground>
         </View>
-    );
-}
+      </View>
+    </ScrollView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-})
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  carouselContentContainer: {
+    flex: 1,
+    backgroundColor: "#000",
+    height: 720,
+    paddingHorizontal: 14,
+  },
+  ImageBg: {
+    flex: 1,
+    height: null,
+    width: null,
+    opacity: 1,
+    justifyContent: "flex-start",
+  },
+});
 
 export default home;
-
