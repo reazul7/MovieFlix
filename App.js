ceef4caa0854739d8ec94cd2a96614b8a167d337
reazul7 <islamreazul7777@gmail.com>
2021-06-04 18:19:49 +0600

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import {MaterialCommunityIcon} from '@expo/vector-icons'
+
+import Home from './screens/home'
+import Profile from './screens/profile'
+import Recents from './screens/recents'
+
+const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Tab.Navigator 
+      initialRoute = 'Home'
+      activeColor = "#02ad94"
+      inactiveColor = "#dedede"
+      style = {{backgroundColor: '#000'}}
+      barStyle = {{backgroundColor: '#0f0f0f', padding: 4}}
+    >
+      <Tab.Screen 
+        name="Home"
+        component={Home} 
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({color}) => (
+            <MaterialCommunityIcon name='home' color={color} size={28}/>
+          )
+        }}
+        />
+      <Tab.Screen
+         name="Profile" 
+         component={Profile} 
+         options={{
+          tabBarLabel: '',
+          tabBarIcon: ({color}) => (
+            <MaterialCommunityIcon name='camera-metering-spot' color={color} size={28}/>
+          )
+        }}
+         />
+      <Tab.Screen 
+        name="Recents" 
+        component={Recents} 
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({color}) => (
+            <MaterialCommunityIcon name='home' color={color} size={28}/>
+          )
+        }}
+        />
+    </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
